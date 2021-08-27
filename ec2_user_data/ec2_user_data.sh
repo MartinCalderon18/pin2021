79ec96d1a232f802f14bf420931eab687748341b
Martin Calderon <fc2003@gmail.com>
2021-08-27 18:05:06 -0300

    Initial Commit

---
+++ ec2_user_data/ec2_user_data.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Update package manager sources
+sudo apt-get update
+#Install unzip
+sudo apt install unzip
+# Download AWS CLI https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# Unzip and install the AWS CLI Console
+unzip awscliv2.zip
+sudo ./aws/install
+# Download EKS CLI https://github.com/weaveworks/eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+# Move eksctl to /usr/local/bin
+sudo mv /tmp/eksctl /usr/local/bin
+
+# Donwload last version of kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# Install kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#Install Docker from snap
+sudo snap install docker
+
+# Adding Docker group and setting permissions
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+
+# Install HELM (Debian/Ubuntu) https://helm.sh/es/docs/intro/install/
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+# aws IAM Authenticator
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+
