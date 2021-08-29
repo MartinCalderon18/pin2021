5db91d1e8b88c85ec4b81bca94a66d48d6fb8dbe
Martin Calderon <fc2003@gmail.com>
2021-08-29 20:35:24 -0300

    added cleanup EFK and prometheus-grafana-deploy.sh

---
+++ monitoring/cleanup-EFK.sh
@@ -0,0 +1,26 @@
+cd  ~/environment/
+
+kubectl delete -f ~/environment/logging/fluentbit.yaml
+
+aws es delete-elasticsearch-domain \
+    --domain-name ${ES_DOMAIN_NAME}
+
+eksctl delete iamserviceaccount \
+    --name fluent-bit \
+    --namespace logging \
+    --cluster eksworkshop-eksctl \
+    --wait
+
+aws iam delete-policy   \
+  --policy-arn "arn:aws:iam::${ACCOUNT_ID}:policy/fluent-bit-policy"
+
+kubectl delete namespace logging
+
+rm -rf ~/environment/logging
+
+unset ES_DOMAIN_NAME
+unset ES_VERSION
+unset ES_DOMAIN_USER
+unset ES_DOMAIN_PASSWORD
+unset FLUENTBIT_ROLE
+unset ES_ENDPOINT
