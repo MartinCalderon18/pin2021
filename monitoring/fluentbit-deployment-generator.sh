91335900e38cff4adbd5fd2d6ea57f9dc18a1322
Martin Calderon <fc2003@gmail.com>
2021-08-29 16:31:26 -0300

    added fluentbit-deployment-generator.sh and fluentbit.deployment.yaml

---
+++ monitoring/fluentbit-deployment-generator.sh
@@ -0,0 +1,7 @@
+cd ~/environment/logging
+
+# get the Elasticsearch Endpoint
+export ES_ENDPOINT=$(aws es describe-elasticsearch-domain --domain-name ${ES_DOMAIN_NAME} --output text --query "DomainStatus.Endpoint")
+
+curl -Ss https://www.eksworkshop.com/intermediate/230_logging/deploy.files/fluentbit.yaml \
+    | envsubst > ~/environment/logging/fluentbit.yaml
