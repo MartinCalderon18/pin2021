ce7de080dd08eb47c278c26b43c597ae07b554b1
Matias Casoli <mcasoli@linets.cl>
2022-11-02 13:58:20 -0300

    Upgrade eks module version

--- eks_setup_terraform/eks-cluster.tf
+++ eks_setup_terraform/eks-cluster.tf
@@ -1,8 +1,12 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "18.26.6"
   cluster_name    = local.cluster_name
   cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets
+  
+  #Cluster Networks
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids     = module.vpc.private_subnets
 
   tags = {
     Environment = "training"
@@ -10,30 +14,33 @@ module "eks" {
     GithubOrg   = "terraform-aws-modules"
   }
 
-  vpc_id = module.vpc.vpc_id
-
-  workers_group_defaults = {
+  eks_managed_node_group_defaults = {
     root_volume_type = "gp2"
+    instance_types = ["t2.small"]
   }
+  eks_managed_node_groups = {
+    one = {
+      name                    = "worker-group-1"
+      instance_type           = "t2.small"
+      desired_size            = 2
+      pre_bootstrap_user_data = <<-EOT
+      echo 'foo bar'
+      EOT
+       vpc_security_group_ids = [aws_security_group.worker_group_mgmt_one.id ]
+    }
 
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    },
-    # {
-    #   name                          = "worker-group-2"
-    #   instance_type                 = "t2.medium"
-    #   additional_userdata           = "echo foo bar"
-    #   additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-    #   asg_desired_capacity          = 1
-    # },
-  ]
+    #two = {
+    #  name                    = "worker-group-2"
+    #  instance_type           = "t2.medium"
+    #  desired_size            = 1
+    #  pre_bootstrap_user_data = <<-EOT
+    #  echo 'foo bar'
+    #  EOT
+    #   vpc_security_group_ids = [aws_security_group.worker_group_mgmt_two.id] ]
+    #}
+  }
 }
-
+  
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
