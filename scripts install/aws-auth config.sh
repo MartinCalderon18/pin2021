fcdbe2f19181330bfe5fa147b66a3d5f70565ae3
Cristhian Apolo Cevallos <cristhianapolo.94@gmail.com>
2023-07-23 17:20:11 -0500

    config arn

--- scripts install/aws-auth config.sh
+++ scripts install/aws-auth config.sh
@@ -3,7 +3,7 @@ kubectl edit -n kube-system configmap/aws-auth
 
 #Agregar a la altura de mapRoles
   mapUsers: |
-    - userarn: arn:aws:iam::265198653890:user/caapolo
-      username: caapolo
+    - userarn: arn:aws:iam::265198653890:user/grupo-13
+      username: grupo-13
       groups:
       - system:masters
